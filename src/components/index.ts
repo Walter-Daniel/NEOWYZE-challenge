569c9efdcbc9cf139c9f713f2ffc108c098fa274
Walter <walterdcarrizo.19@gmail.com>
2024-05-14 00:50:34 -0300

    add(-): create navbar and active link

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,5 @@
+export { Navbar } from './Navbar';
 export { Pagination } from './Pagination';
-
 export { CardCharacter } from './CardCharacter';
-
 export { CharacterList } from './CharacterList';
-
 export { Card } from './CardMovie';
