a27d6a82334caf8e51f40b256f20a38520dbbf16
Walter <walterdcarrizo.19@gmail.com>
2024-05-14 10:30:26 -0300

    fix(-): Resolved issues with movies and character page layout for mobile devices

--- src/app/(general)/movies/page.tsx
+++ src/app/(general)/movies/page.tsx
@@ -6,7 +6,7 @@ export default async function MoviesPage() {
   const movies = await getMovies();
 
   return (
-    <div>
+    <div className='pb-4'>
       <h2 className='font-jedi text-3xl md:text-7xl text-center py-6 text-yellow-300'>Movies</h2>
       <div className="flex items-center justify-center flex-wrap min-h-screen">
         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
