30077cd8425bf4d629080709719e0b696c79650b
Walter <walterdcarrizo.19@gmail.com>
2024-05-14 10:25:06 -0300

    add(-): create footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='h-10 text-center'>
+        <span className='font-semibold'>©All rights reserved</span>
+    </div>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { Footer } from './Footer';
 export { Navbar } from './Navbar';
 export { Pagination } from './Pagination';
 export { CardCharacter } from './CardCharacter';
